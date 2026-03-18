a9fdaabe4b201844d3391ec3ed6af13ccd611de9
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2026-03-18 12:42:36 -0400

    Remove unneeded imports

--- ctdscreenie-neo/src/main/java/mastergeneral156/ctd/screenie/CTDScreenieClient.java
+++ ctdscreenie-neo/src/main/java/mastergeneral156/ctd/screenie/CTDScreenieClient.java
@@ -2,18 +2,14 @@ package mastergeneral156.ctd.screenie;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.Screenshot;
-import net.minecraft.network.chat.Component;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
-
-import java.io.File;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
 @Mod(value = CTDScreenie.MODID, dist = Dist.CLIENT)
