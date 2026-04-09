7a03fd900da34d582145c43775c999a7057b84f9
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2026-04-08 22:00:39 -0400

    Remove neoforge mdk example comments lmao

--- ctdscreenie-neo/src/main/java/mastergeneral156/ctd/screenie/CTDScreenie.java
+++ ctdscreenie-neo/src/main/java/mastergeneral156/ctd/screenie/CTDScreenie.java
@@ -9,19 +9,12 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(CTDScreenie.MODID)
 public class CTDScreenie {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "ctdscreenie";
-    // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public CTDScreenie(IEventBus modEventBus, ModContainer modContainer) {
-
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.CLIENT, Config.CLIENT_SPEC);
     }
 }
